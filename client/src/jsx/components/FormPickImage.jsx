1fece54f0465b7486b7f17e0ef33e49ecf013d51
issam-seghir <issamusma@hotmail.com>
2024-01-13 16:35:16 +0100

    complete custome input file component

--- client/src/jsx/components/FormPickImage.jsx
+++ client/src/jsx/components/FormPickImage.jsx
@@ -26,9 +26,10 @@ const FILE_TYPE_MAP = {
 export default function PickAvatar({ name, errors, register, type, multiple }) {
 	const { onChange, ref } = register(name);
 	const [image, setImage] = useState();
+	const [file, setFile] = useState(null);
 	const [errorMessage, setErrorMessage] = useState(null);
 	const { palette } = useTheme();
-
+console.log(file);
 	const onAvatarChange = useCallback(async (event) => {
 		const file = event.target.files[0];
 		if (file) {
@@ -39,7 +40,9 @@ export default function PickAvatar({ name, errors, register, type, multiple }) {
 			}
 			const base64 = await getBase64(file);
 
+			setErrorMessage(null); // Clear the error message
 			setImage(base64);
+			setFile(file);
 			onChange(event);
 		}
 	}, []);
@@ -59,26 +62,25 @@ export default function PickAvatar({ name, errors, register, type, multiple }) {
 				/>
 
 				<Box p="1rem" display={"block"} component="label" htmlFor={name} sx={{ "&:hover": { cursor: "pointer" } }}>
-					{image ? (
-						<FlexBetween gap={1}>
-							<Typography noWrap width={"280px"} variant="body1" color={palette.grey[700]}>
-								{image?.name}
-							</Typography>
-							<Typography variant="body1" color={palette.primary.main}>{`${byteToMb(image?.size)} mb`}</Typography>
-							<EditOutlinedIcon />
-						</FlexBetween>
+					{errors[name] || errorMessage ? (
+						<Typography variant="body1" color={palette.grey[600]}>
+							{errors[name]?.message || errorMessage}
+						</Typography>
+					) : file ? (
+							<FlexBetween gap={1}>
+								<Typography noWrap width={"280px"} variant="body1" color={palette.grey[700]}>
+									{file.name}
+								</Typography>
+								<Box display={"flex"} gap={3}>
+									<Typography variant="body1" color={palette.primary.main}>{`${byteToMb(file.size)} mb`}</Typography>
+									<EditOutlinedIcon />
+								</Box>
+							</FlexBetween>
 					) : (
 						<Typography variant="body1" color={palette.grey[500]}>
 							Drag here, or click to select Picture{" "}
 						</Typography>
 					)}
-					{image && <img src={image} width="100px" alt="img" />}
-					{errors[name] ||
-						(errorMessage && (
-							<Typography variant="body1" color={palette.grey[600]}>
-								{errors[name]?.message || errorMessage}
-							</Typography>
-						))}
 				</Box>
 			</Box>
 		</Box>

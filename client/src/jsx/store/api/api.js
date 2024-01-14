bbd683e46c99b5b49b9cf199709660af2b04ad81
issam-seghir <issamusma@hotmail.com>
2024-01-15 00:10:21 +0100

    add user components

--- client/src/jsx/store/api/api.js
+++ client/src/jsx/store/api/api.js
@@ -20,21 +20,13 @@ const baseQueryWithReauth = async (args, api, extraOptions) => {
 	let result = await baseQuery(args, api, extraOptions);
 console.log(result);
 	if (result?.error?.originalStatus === 403) {
-		console.log("sending refresh token");
 		// send refresh token to get new access token
 		const refreshResult = await baseQuery("/refresh", api, extraOptions);
 		if (refreshResult?.data) {
-			console.log("refreshResult");
-			console.log(refreshResult);
 			const newToken = refreshResult.data.accessToken;
-			console.log(newToken);
 			const user = api.getState().auth.user;
-			const token = api.getState().auth.token;
-			console.log(user);
 			// store the new token
 			api.dispatch(setCredentials({ token: newToken, user }));
-			console.log(user);
-			console.log(token);
 			// retry the original query with new access token
 			result = await baseQuery(args, api, extraOptions);
 		} else {

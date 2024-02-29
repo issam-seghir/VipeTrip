f050b18d372b5d53f0898392257d9d93dfc63d6a
Seghir Issam <issamusma@hotmail.com>
2024-02-29 12:20:48 +0100

    add login page styling

--- client/src/jsx/App.jsx
+++ client/src/jsx/App.jsx
@@ -1,5 +1,9 @@
 import { arLocale } from "@data/localization/ar.js";
-import { selectLocal, selectMode, selectTheme } from "@jsx/store/slices/globalSlice";
+import {
+  selectLocal,
+  selectMode,
+  selectTheme,
+} from "@jsx/store/slices/globalSlice";
 import { PrimeReactProvider, addLocale } from "primereact/api";
 import { useDispatch, useSelector } from "react-redux";
 import { Outlet } from "react-router-dom";
@@ -7,32 +11,32 @@ import { Outlet } from "react-router-dom";
 addLocale("ar", arLocale);
 
 function App() {
-	const mode = useSelector(selectMode);
-	// const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
-	const theme = useSelector(selectTheme);
-	const local = useSelector(selectLocal);
-	const dispatch = useDispatch();
-	const primereactConfig = {
-		// inputStyle: "filled",
-		//  zIndex: {
-		//     modal: 1100,    // dialog, sidebar
-		//     overlay: 1000,  // dropdown, overlaypanel
-		//     menu: 1000,     // overlay menus
-		//     tooltip: 1100   // tooltip
-		// },
-		// unstyled: true, //  removes all of components styles in the core
-		// locale: "en",
-		locale: local,
-		ripple: false,
-	};
+  const mode = useSelector(selectMode);
+  // const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+  const theme = useSelector(selectTheme);
+  const local = useSelector(selectLocal);
+  const dispatch = useDispatch();
+  const primereactConfig = {
+    // inputStyle: "filled",
+    //  zIndex: {
+    //     modal: 1100,    // dialog, sidebar
+    //     overlay: 1000,  // dropdown, overlaypanel
+    //     menu: 1000,     // overlay menus
+    //     tooltip: 1100   // tooltip
+    // },
+    // unstyled: true, //  removes all of components styles in the core
+    // locale: "en",
+    locale: local,
+    ripple: false,
+  };
 
-	return (
-		// <ThemeProvider theme={theme}>
-		<PrimeReactProvider value={primereactConfig}>
-			<Outlet />
-		</PrimeReactProvider>
-		// </ThemeProvider>
-	);
+  return (
+    // <ThemeProvider theme={theme}>
+    <PrimeReactProvider value={primereactConfig}>
+      <Outlet />
+    </PrimeReactProvider>
+    // </ThemeProvider>
+  );
 }
 
 export default App;
--- client/src/jsx/pages/Login.jsx
+++ client/src/jsx/pages/Login.jsx
@@ -3,36 +3,61 @@ import { Settings } from "@pages/Settings";
 import { Checkbox } from "primereact/checkbox";
 import { Button } from "primereact/button";
 import React, { useState } from "react";
-import {  classNames } from "primereact/utils";
+import { classNames } from "primereact/utils";
+import sectionImg from "@assets/images/Autumn Biking Companions.png";
+
+import { Image } from "primereact/image";
 
 export default function Login() {
-	    const [ingredients, setIngredients] = useState([]);
+  const [ingredients, setIngredients] = useState([]);
 
-		const onIngredientsChange = (e) => {
-			let _ingredients = [...ingredients];
+  const onIngredientsChange = (e) => {
+    let _ingredients = [...ingredients];
 
-			if (e.checked) _ingredients.push(e.value);
-			else _ingredients.splice(_ingredients.indexOf(e.value), 1);
+    if (e.checked) _ingredients.push(e.value);
+    else _ingredients.splice(_ingredients.indexOf(e.value), 1);
 
-			setIngredients(_ingredients);
-		};
-	return (
-
-<div className="grid grid-nogutter surface-0 text-800">
-    <div className="col-12 md:col-6 p-6 text-center md:text-left flex align-items-center ">
+    setIngredients(_ingredients);
+  };
+  return (
+    <div className="flex h-screen overflow-hidden">
+      <div
+        className=" p-6 text-center md:text-left flex align-items-center"
+        style={{ flex: "50%" }}
+      >
         <section>
-            <span className="block text-6xl font-bold mb-1">Create the screens</span>
-            <div className="text-6xl text-primary font-bold mb-3">your visitors deserve to see</div>
-            <p className="mt-0 mb-4 text-700 line-height-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-
-            <Button label="Learn More" type="button" className="mr-3 p-button-raised" />
-            <Button label="Live Demo" type="button" className="p-button-outlined" />
+          <span className="block text-6xl font-bold mb-1">
+            Welcome To VipeTrip
+          </span>
+          <div className="text-6xl  text-primary font-bold mb-3">
+            your trip begien here !!
+          </div>
+          <div className="text-2xl  text-primary font-bold mb-3">Sign In</div>
         </section>
+      </div>
+      <div
+        style={{
+          flex: "50%",
+        }}
+      >
+        <img
+          src={sectionImg}
+          alt="section"
+          style={{
+            clipPath: "polygon(8% 0, 100% 0%, 100% 100%, 0 100%)",
+            objectFit: "cover",
+          }}
+        />
+        {/* <Image
+          style={{ flex: "50%" }}
+          imageStyle={{
+            clipPath: "polygon(8% 0, 100% 0%, 100% 100%, 0 100%)",
+            objectFit: "cover",
+          }}
+          src={sectionImg}
+          alt="section"
+        /> */}
+      </div>
     </div>
-    <div className="col-12 md:col-6 overflow-hidden">
-        <img src="/demo/images/blocks/hero/hero-1.png" alt="hero-1" className="md:ml-auto block md:h-full" style={{ clipPath: 'polygon(8% 0, 100% 0%, 100% 100%, 0 100%)' }} />
-    </div>
-</div>
-
-	);
+  );
 }

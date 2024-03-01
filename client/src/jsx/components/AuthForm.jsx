b7550c0d15f736a480de7ba7101ab88c0bd58486
issam-seghir <issamusma@hotmail.com>
2024-03-01 23:55:19 +0100

    fix primereact css layer

--- client/src/jsx/components/AuthForm.jsx
+++ client/src/jsx/components/AuthForm.jsx
@@ -92,10 +92,9 @@ export  function AuthForm() {
 			<Toast ref={toast} />
 
 			<form onSubmit={handleSubmit(onSubmit)}>
-
-					<PFormTextField defaultValue={"admin@test.com"} name={"email"} label="Email" control={control} errorMessage={getServerErrorMessageForField("email")} />
-					{/* <FormTextField defaultValue={"admin@test.com"} name={"email"} label="Email" control={control} errorMessage={getServerErrorMessageForField("email")} sx={{ gridColumn: "span 4" }} /> */}
-					{/* <FormTextField defaultValue={"123456@Admin"} name={"password"} label="Password" type="password" errorMessage={getServerErrorMessageForField("password")} control={control} sx={{ gridColumn: "span 4" }} /> */}
+				<PFormTextField control={control} defaultValue={"admin@test.com"} name={"email"} label="Email" type="email" size={"p-inputtext-lg"} errorMessage={getServerErrorMessageForField("email")} />
+				{/* <FormTextField defaultValue={"admin@test.com"} name={"email"} label="Email" control={control} errorMessage={getServerErrorMessageForField("email")} sx={{ gridColumn: "span 4" }} /> */}
+				{/* <FormTextField defaultValue={"123456@Admin"} name={"password"} label="Password" type="password" errorMessage={getServerErrorMessageForField("password")} control={control} sx={{ gridColumn: "span 4" }} /> */}
 
 				{/* BUTTONS */}
 				<Box>
--- client/src/jsx/pages/Login.jsx
+++ client/src/jsx/pages/Login.jsx
@@ -54,17 +54,8 @@ export default function Login() {
 							to <span className="text-primary text-animate">VipeTrip</span>
 						</div>
 					</div>
+					<AuthForm />
 					<div>
-						<label htmlFor="email" className="block text-900 font-medium mb-2">
-							Email
-						</label>
-						<InputText id="email" type="email" placeholder="example@mail.com" className="w-full mb-3" />
-
-						<label htmlFor="password" className="block text-900 font-medium mb-2">
-							Password
-						</label>
-						<InputText id="password" type="password" placeholder="Password" className="w-full mb-3" />
-
 						<div className="flex align-items-center justify-content-between mb-6">
 							<div className="flex align-items-center">
 								<Checkbox id="rememberme" onChange={(e) => setChecked(e.checked)} checked={checked} className="mr-2" />
@@ -75,7 +66,6 @@ export default function Login() {
 
 						<Button label="Sign In" icon="pi pi-user" className="w-full" />
 					</div>
-					<AuthForm />
 				</div>
 				{/* <section>
 				<span className="text-600 font-medium line-height-3">Don&apos;t have an account?</span>

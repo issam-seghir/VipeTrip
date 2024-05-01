dd4c55310cd136d855a2762cd0f18f2b0acc95c8
issam-seghir <issamusma@hotmail.com>
2024-05-01 23:19:17 +0100

    add multer validation

--- server/routes/api/v1/posts.js
+++ server/routes/api/v1/posts.js
@@ -1,15 +1,23 @@
 const express = require("express");
 
 const router = express.Router();
-const { getFeedPosts, getUserPosts, sharePost, getSinglePost, createPost, deletePost, updatePost } = require("@controllers/postsController");
+const {
+	getFeedPosts,
+	getUserPosts,
+	sharePost,
+	getSinglePost,
+	createPost,
+	deletePost,
+	updatePost,
+} = require("@controllers/postsController");
 const multerErrorHandler = require("@/middleware/multer/multerErrorHandler");
 const { upload, uploadPost } = require("@/middleware/multer/multerUploader");
 const validate = require("express-zod-safe");
-const {
-	createPostSchema
-} = require("@validations/postSchema");
+const { createPostSchema } = require("@validations/postSchema");
 
-router.route("/").post( uploadPost.array("images", 5), createPost);
+router
+	.route("/")
+	.post(uploadPost.array("images", 5), multerErrorHandler(upload), validate(createPostSchema), createPost);
 router.route("/").get(getFeedPosts);
 router.route("/:postId").get(getSinglePost).delete(deletePost).put(updatePost);
 
